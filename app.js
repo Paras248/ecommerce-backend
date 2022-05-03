b4c378acae18e4dc021e6be0f9590fccd9f5c1cb
Paras248 <paraspatil248@gmail.com>
2022-05-03 22:21:34 +0530

    added customError.js swagger.yaml and modified app.js

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
-//import all routes here
+// for swagger documentation
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+// import all routes here
 const home = require("./routes/home");
 
-//router middleware
+// swagger middleware
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// regular middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookies and fileUpload middleware
+app.use(cookieParser());
+app.use(fileUpload());
+
+//morgan middleware
+app.use(morgan("tiny"));
+
+// router middleware
 app.use("/api/v1", home);
 
 // exports app.js
