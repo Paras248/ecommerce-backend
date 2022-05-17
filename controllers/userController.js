1cabdee118b0cbe3bd0e234336728949ad0a787f
Paras248 <paraspatil248@gmail.com>
2022-05-18 01:10:50 +0530

    Added logout feature and modified swagger.yaml for logout route

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,3 +66,14 @@ exports.login = BigPromise(async (req, res, next) => {
 
 	cookieToken(user, res);
 });
+
+exports.logout = BigPromise(async (req, res, next) => {
+	res.cookie("token", null, {
+		expires: new Date(Date.now()),
+		httpOnly: true,
+	});
+	res.status(200).json({
+		success: true,
+		message: "You have logged out successfully!!!",
+	});
+});
